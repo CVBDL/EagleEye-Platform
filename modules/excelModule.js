837e4531f4989353f45b854f6b3cb9a74653046b
Merlin Yongfeng Mo <MMo2@rockwellautomation.com>
2016-06-20 14:03:59 +0800

    Add excel module

---
+++ modules/excelModule.js
@@ -0,0 +1,19 @@
+/**
+ * Created by MMo2 on 6/20/2016.
+ */
+
+exports.write = function() {
+
+}
+
+exports.read = function() {
+
+}
+
+exports.remove = function() {
+
+}
+
+exports.import = function() {
+
+}

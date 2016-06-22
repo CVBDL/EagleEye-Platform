76fd314df5147412d57dd076b3cb8c926ab8f345
zhongsp <patrick.zhongsp@gmail.com>
2016-06-22 10:02:19 +0800

    Edit chart set unit tests

--- tests/modules/testChartSetModule.js
+++ tests/modules/testChartSetModule.js
@@ -6,9 +6,9 @@ var should = require('should')
     , DB = require('../../helpers/dbHelper')
     , fixtures = require('../fixtures/chartSetModule');
 
-var chartModule = require('../../modules/chartSetModule');
+var chartSetModule = require('../../modules/chartSetModule');
 
-describe('Model chart Tests', function() {
+describe('Model chart set tests', function() {
 
     var chart = {
       "title": "Chart set sample",
@@ -29,15 +29,15 @@ describe('Model chart Tests', function() {
     })
 
     it('all', function(done) {
-        chartModule.all(function(err, docs) {
+        chartSetModule.all(function(err, docs) {
             docs.length.should.eql(2);
             done();
         })
     })
 
     it('clear', function(done) {
-        chartModule.clearCollection(function(err) {
-            chartModule.all(function(err, result) {
+        chartSetModule.clearCollection(function(err) {
+            chartSetModule.all(function(err, result) {
                 result.length.should.eql(0);
                 done();
             })
@@ -45,8 +45,8 @@ describe('Model chart Tests', function() {
     })
 
     it('create', function(done) {
-        chartModule.create(chart, function(err, id) {
-            chartModule.all(function(err, docs) {
+        chartSetModule.create(chart, function(err, id) {
+            chartSetModule.all(function(err, docs) {
                 docs.length.should.eql(3);
                 for (var key in fixtures.collections.chart_set_collection[0]) {
                     docs[2][key].should.eql(chart[key]);
@@ -57,8 +57,8 @@ describe('Model chart Tests', function() {
     })
 
     it('getOne: id', function(done) {
-        chartModule.create(chart, function(err, id) {
-            chartModule.getOne(id, function(err, docs) {
+        chartSetModule.create(chart, function(err, id) {
+            chartSetModule.getOne(id, function(err, docs) {
                 docs.length.should.eql(1);
                 for (var key in fixtures.collections.chart_set_collection[0]) {
                     docs[0][key].should.eql(chart[key]);
@@ -69,7 +69,7 @@ describe('Model chart Tests', function() {
     })
 
     it('getOne: friendlyUrl', function(done) {
-        chartModule.getOne(fixtures.collections.chart_set_collection[0].friendlyUrl, function(err, docs) {
+        chartSetModule.getOne(fixtures.collections.chart_set_collection[0].friendlyUrl, function(err, docs) {
             docs.length.should.eql(1);
             for (var key in fixtures.collections.chart_set_collection[0]) {
                 docs[0][key].should.eql(fixtures.collections.chart_set_collection[0][key]);
@@ -79,9 +79,9 @@ describe('Model chart Tests', function() {
     })
 
     it('remove', function(done) {
-        chartModule.all(function(err, docs) {
-            chartModule.remove(docs[0]._id, function(err) {
-                chartModule.all(function(err, result) {
+        chartSetModule.all(function(err, docs) {
+            chartSetModule.remove(docs[0]._id, function(err) {
+                chartSetModule.all(function(err, result) {
                     result.length.should.eql(1);
                     result[0]._id.should.not.eql(docs[0]._id);
                     done();
---
+++ unitTest.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+node_modules/.bin/_mocha tests/modules/fullSuitTests.js

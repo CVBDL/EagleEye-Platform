ea08560bb3b3aaf866b7e523e3dfac296776b903
zhongsp <patrick.zhongsp@gmail.com>
2016-07-06 15:56:14 +0800

    chart default options

--- helpers/defaultChartOptions.js
+++ helpers/defaultChartOptions.js
@@ -1,155 +1,154 @@
 var LineChartOptions = {
     animation: {
         startup: true,
-        duration: 1500,
-        easing: 'inAndOut'
-    },
-    backgroundColor: {
-        stroke: '#666',
-        strokeWidth: 0,
-        fill: '#ffffff'
-    },
-    chartArea: {
-        backgroundColor: {
-            stroke: '#666',
-            strokeWidth: 2
-        },
-        left: 100
-    },
-    crosshair: {
-        color: '#006600',
-        opacity: 0.3,
-        orientation: 'horizontal',
-        trigger: 'both'
-    },
-    hAxis: {
-        gridlines: {
-            color: '#DDDDDD'
-        },
-        viewWindow: {}
-    },
-    vAxis: {
-        gridlines: {
-            color: '#DDDDDD'
-        },
-        viewWindow: {}
-    },
-    legend: {
-        alignment: 'start',
-        position: 'right',
-        textStyle: {
-            italic: true
-        }
-    },
-    lineWidth: 3,
-    pointShape: 'diamond',
-    tooltip: {
-        ignoreBounds: false,
-        isHtml: false,
-        textStyle: {
-            color: '#66CC66',
-            fontSize: 12,
-            italic: true
-        }
+        duration: 500,
+        easing: 'linear'
     }
+    // backgroundColor: {
+    //     stroke: '#666',
+    //     strokeWidth: 0,
+    //     fill: '#ffffff'
+    // },
+    // chartArea: {
+    //     backgroundColor: {
+    //         stroke: '#666',
+    //         strokeWidth: 2
+    //     },
+    //     left: 100
+    // },
+    // crosshair: {
+    //     color: '#006600',
+    //     opacity: 0.3,
+    //     orientation: 'horizontal',
+    //     trigger: 'both'
+    // },
+    // hAxis: {
+    //     gridlines: {
+    //         color: '#DDDDDD'
+    //     },
+    //     viewWindow: {}
+    // },
+    // vAxis: {
+    //     gridlines: {
+    //         color: '#DDDDDD'
+    //     },
+    //     viewWindow: {}
+    // },
+    // legend: {
+    //     alignment: 'start',
+    //     position: 'right',
+    //     textStyle: {
+    //         italic: true
+    //     }
+    // },
+    // lineWidth: 3,
+    // pointShape: 'diamond',
+    // tooltip: {
+    //     ignoreBounds: false,
+    //     isHtml: false,
+    //     textStyle: {
+    //         color: '#66CC66',
+    //         fontSize: 12,
+    //         italic: true
+    //     }
+    // }
 };
 
 var ColumnChartOptions = {
     animation: {
         startup: true,
-        duration: 1500,
-        easing: 'inAndOut'
-    },
-    backgroundColor: {
-        stroke: '#666',
-        strokeWidth: 0,
-        fill: '#ffffff'
-    },
-    bar: {groupWidth: "75%"},
-    chartArea: {
-        backgroundColor: {
-            stroke: '#666',
-            strokeWidth: 2
-        },
-        left: 100
-    },
-    hAxis: {
-        gridlines: {
-            color: '#DDDDDD'
-        },
-        viewWindow: {}
-    },
-    vAxis: {
-        gridlines: {
-            color: '#DDDDDD'
-        },
-        viewWindow: {}
-    },
-    legend: {
-        alignment: 'start',
-        position: 'right',
-        textStyle: {
-            italic: true
-        }
-    },
-    tooltip: {
-        ignoreBounds: false,
-        isHtml: false,
-        textStyle: {
-            color: '#66CC66',
-            fontSize: 12,
-            italic: true
-        }
+        duration: 500,
+        easing: 'linear'
     }
+    // backgroundColor: {
+    //     stroke: '#666',
+    //     strokeWidth: 0,
+    //     fill: '#ffffff'
+    // },
+    // bar: {groupWidth: "75%"},
+    // chartArea: {
+    //     backgroundColor: {
+    //         stroke: '#666',
+    //         strokeWidth: 2
+    //     },
+    //     left: 100
+    // },
+    // hAxis: {
+    //     gridlines: {
+    //         color: '#DDDDDD'
+    //     },
+    //     viewWindow: {}
+    // },
+    // vAxis: {
+    //     gridlines: {
+    //         color: '#DDDDDD'
+    //     },
+    //     viewWindow: {}
+    // },
+    // legend: {
+    //     alignment: 'start',
+    //     position: 'right',
+    //     textStyle: {
+    //         italic: true
+    //     }
+    // },
+    // tooltip: {
+    //     ignoreBounds: false,
+    //     isHtml: false,
+    //     textStyle: {
+    //         color: '#66CC66',
+    //         fontSize: 12,
+    //         italic: true
+    //     }
+    // }
 };
 var BarChartOptions = {
     animation: {
         startup: true,
-        duration: 1500,
-        easing: 'inAndOut'
-    },
-
-    backgroundColor: {
-        stroke: '#666',
-        strokeWidth: 0,
-        fill: '#ffffff'
-    },
-    bar: {groupWidth: "75%"},
-    chartArea: {
-        backgroundColor: {
-            stroke: '#666',
-            strokeWidth: 2
-        },
-        left: 100
-    },
-    hAxis: {
-        gridlines: {
-            color: '#DDDDDD'
-        },
-        viewWindow: {}
-    },
-    vAxis: {
-        gridlines: {
-            color: '#DDDDDD'
-        },
-        viewWindow: {}
-    },
-    legend: {
-        alignment: 'start',
-        position: 'right',
-        textStyle: {
-            italic: true
-        }
-    },
-    tooltip: {
-        ignoreBounds: false,
-        isHtml: false,
-        textStyle: {
-            color: '#66CC66',
-            fontSize: 12,
-            italic: true
-        }
+        duration: 500,
+        easing: 'linear'
     }
+    // backgroundColor: {
+    //     stroke: '#666',
+    //     strokeWidth: 0,
+    //     fill: '#ffffff'
+    // },
+    // bar: {groupWidth: "75%"},
+    // chartArea: {
+    //     backgroundColor: {
+    //         stroke: '#666',
+    //         strokeWidth: 2
+    //     },
+    //     left: 100
+    // },
+    // hAxis: {
+    //     gridlines: {
+    //         color: '#DDDDDD'
+    //     },
+    //     viewWindow: {}
+    // },
+    // vAxis: {
+    //     gridlines: {
+    //         color: '#DDDDDD'
+    //     },
+    //     viewWindow: {}
+    // },
+    // legend: {
+    //     alignment: 'start',
+    //     position: 'right',
+    //     textStyle: {
+    //         italic: true
+    //     }
+    // },
+    // tooltip: {
+    //     ignoreBounds: false,
+    //     isHtml: false,
+    //     textStyle: {
+    //         color: '#66CC66',
+    //         fontSize: 12,
+    //         italic: true
+    //     }
+    // }
 };
 exports.chartOptions = {
     LineChart: LineChartOptions,

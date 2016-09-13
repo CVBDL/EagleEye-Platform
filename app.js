592a022315de1a8a06077ebfe4251fbfa3507e91
zhongsp <patrick.zhongsp@gmail.com>
2016-09-13 16:55:25 +0800

    refactor

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var scheduleTask = require('./routes/schedule-management');
 
 var app = express();
 
+// CORS support
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -25,14 +26,12 @@ app.use(function(req, res, next) {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(multipart({
   uploadDir: "./excelPath/prod"
 }));

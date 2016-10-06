19eaedd9f688c05c72dfdd8d35faf85f8a80faff
zhongsp <patrick.zhongsp@gmail.com>
2016-10-06 13:40:45 +0800

    edit ETL

--- modules/etl.js
+++ modules/etl.js
@@ -7,8 +7,9 @@
 let DB    = require('../helpers/dbHelper');
 let utils = require('../helpers/utils');
 
-let COLLECTION = "chart_collection";
-let IMAGE_CHART_TYPE = "ImageChart";
+let CHART_COLLECTION = 'chart_collection';
+let CHART_SET_COLLECTION = 'chart_set_collection'
+let IMAGE_CHART_TYPE = 'ImageChart';
 
 /**
  * etl.start
@@ -18,8 +19,9 @@ let IMAGE_CHART_TYPE = "ImageChart";
 exports.start = function() {
   let db = DB.get();
 
+  // chart collection
   utils.getRestApiRootEndpoint().then(function(rootEndpoint) {
-    db.collection(COLLECTION).find({}).forEach(function(doc) {
+    db.collection(CHART_COLLECTION).find({}).forEach(function(doc) {
 
       // `browserDownloadUrl`
       doc.browserDownloadUrl = {
@@ -40,7 +42,27 @@ exports.start = function() {
 
       delete doc.lastUpdateTimestamp;
 
-      db.collection(COLLECTION).save(doc);
+      db.collection(CHART_COLLECTION).save(doc);
     });
   });
+
+  // chart set collection
+  db.collection(CHART_SET_COLLECTION).find({}).forEach(function(doc) {
+    // `timestamp` to `createdAt`
+    if (!doc.createdAt) {
+      doc.createdAt = new Date(doc.timestamp).toISOString();
+    }
+
+    delete doc.timestamp;
+
+    // `lastUpdateTimestamp` to `updatedAt`
+    if (!doc.updatedAt) {
+      doc.updatedAt = new Date(doc.lastUpdateTimestamp).toISOString();
+    }
+
+    delete doc.lastUpdateTimestamp;
+
+    db.collection(CHART_SET_COLLECTION).save(doc);
+  });
+
 };

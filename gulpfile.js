3bf28a06fb6dae1b543d92d2012d9aaec1c98888
zhongsp <patrick.zhongsp@gmail.com>
2017-01-25 18:28:53 +0800

    add script for ci unit testing

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+﻿var gulp = require('gulp');
+var exec = require('child_process').exec;
+
+gulp.task('watch', function () {
+  gulp.watch([
+    'helpers/**/*.js',
+    'modules/**/*.js',
+    'routes/**/*.js',
+    'tests/**/*.js',
+    'gulpfile.js'
+   ], ['ci:test']);
+});
+
+gulp.task('ci:test', function (cb) {
+  exec('npm test', function (error, stdout, stderr) {
+    if (!error) {
+      console.log(stdout);
+    } else {
+      console.log(stderr);
+    }
+    cb();
+  })
+});

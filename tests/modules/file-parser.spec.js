af9718b4f41a75f1208e1e0a2190c9dead10d42e
zhongsp <patrick.zhongsp@gmail.com>
2017-02-12 22:24:58 +0800

    unit test

--- tests/modules/file-parser.spec.js
+++ tests/modules/file-parser.spec.js
@@ -11,6 +11,7 @@ let dbClient = require('../../helpers/dbHelper');
 let fileParser = require('../../modules/file-parser');
 let chartsFixtures = require('../fixtures/charts');
 let chartSetsFixtures = require('../fixtures/chart-sets');
+let dataTableFixtures = require('../fixtures/datatable');
 
 const CHART_COLLECTION_NAME = "chart_collection";
 const CHART_SET_COLLECTION_NAME = "chart_set_collection";
@@ -77,5 +78,28 @@ describe('modules: charts', function () {
         })
         .catch(done);
     });
+  });
+
+
+  describe('writeXLSXStream', function () {
+
+    //it('should write data table to xlsx file', function (done) {
+    //  let filename = 'temp.xlsx';
+    //  let testFilePath = path.join(__dirname, '..', 'fixtures', filename);
+    //  let stream = fs.createWriteStream(testFilePath);
+
+    //  fileParser
+    //    .readImageStream(writeXLSXStream, dataTableFixtures)
+    //    .then(function () {
+    //      let savedPath = path.join(
+    //        __dirname, '..', '..', 'public', 'upload', filename);
+
+    //      // check saved uploaded file on server
+    //      should.equal(fs.existsSync(savedPath), true);
+
+    //      done();
+    //    })
+    //    .catch(done);
+    //});
   });
 });
